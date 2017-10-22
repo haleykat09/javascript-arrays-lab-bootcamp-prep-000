04900e4aae1e08dbb7d156014fe04671004e09c6
Haley West <haleykat09@yahoo.com>
2017-10-22 16:04:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens = kittens.slice(-1)
-  return kittens
+  return kittens.slice(0, kittens.length - 1)
 }
 
   function removeFirstKitten() {
